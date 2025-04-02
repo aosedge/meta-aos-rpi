0c328d0ed66ce0e48d891e9d982685b05f28afea
oleksandr benidovskyi <oleksandr_benidovskyi@epam.com>
2025-04-02 17:44:13 +0300

    WIP ON CI

--- meta-aos-rpi-install/recipes-core/initrdscripts/files/install_script.sh
+++ meta-aos-rpi-install/recipes-core/initrdscripts/files/install_script.sh
@@ -67,23 +67,45 @@ wait_for_block_device "$BLOCK_DEVICE"
 
 echo "Preparing the SD card..."
 
-mkdir -p /flash /sd
-mount -t auto /dev/mmcblk0p2 /sd
+mkdir -p /sd1 /sd2 /flash1 /flash3
+mount -t auto /dev/mmcblk0p2 /sd2
 
 echo "Flashing root device..."
 
-gunzip -c /sd/rootfs.img.gz | dd of="/dev/$BLOCK_DEVICE" bs=8096 || echo "Failed to write rootfs.img"
+gunzip -c /sd2/rootfs.img.gz | dd of="/dev/$BLOCK_DEVICE" bs=8096 || echo "Failed to write rootfs.img"
 mkfs.ext4 -F -E lazy_journal_init=1 "/dev/$BLOCK_DEVICE_PARTITION" || echo "Failed to format /dev/$BLOCK_DEVICE_PARTITION"
 
-mount -t auto "/dev/$BLOCK_DEVICE_PARTITION" /flash || echo "Failed to mount /dev/$BLOCK_DEVICE_PARTITION"
+mount -t auto "/dev/$BLOCK_DEVICE_PARTITION" /flash3 || echo "Failed to mount /dev/$BLOCK_DEVICE_PARTITION"
+mount -t auto /dev/mmcblk0p1 /sd1/
+
+if [ -f /sd1/firstrun.sh ]; then
+    mount -t auto "/dev/${BLOCK_DEVICE}1" /flash1/
+    output=$(sed -n "s/.*userconf '\(.*\)' '\(.*\)'.*/\1 \2/p" /sd1/firstrun.sh)
+    user=$(echo "$output" | awk '{print $1}')
+    password=$(echo "$output" | awk '{print $2}')
+
+    echo "User: $user"
+    echo "Password: $password"
+
+    echo "$user:x:1000:1000:$user,,,:/home/$user:/bin/sh" >> /flash1/etc/passwd
+    echo "$user:x:1000:" >> /flash1/etc/group
+    echo "$user:$password:19000:0:99999:7:::" >> /flash1/etc/shadow
+
+    mkdir -p "/flash1/home/$user"
+    chown 1000:1000 "/flash1/home/$user"
+    chmod 700 "/flash1/home/$user"
+    umount /flash1
+fi
+
+umount /sd1
 
 echo "Flashing SD card..."
 
-cp /sd/boot.img.gz /flash || echo "Failed to copy boot.img"
-umount /sd
-gunzip -c /flash/boot.img.gz | dd of=/dev/mmcblk0 bs=8096 || echo "Failed to write boot.img"
+cp /sd2/boot.img.gz /flash3 || echo "Failed to copy boot.img"
+umount /sd2
+gunzip -c /flash3/boot.img.gz | dd of=/dev/mmcblk0 bs=8096 || echo "Failed to write boot.img"
 
-rm /flash/boot.img.gz
+rm /flash3/boot.img.gz
 
 echo
 echo "********************************************************************************"
