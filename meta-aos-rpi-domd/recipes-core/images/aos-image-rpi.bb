15343728f7ee5dd7868ec238a6a3b20112ea265c
oleksandr benidovskyi <oleksandr_benidovskyi@epam.com>
2025-04-09 10:11:52 +0300

    [install-script] Allow use username and password from pri-imager
    Signed-off-by: Oleksandr Benidovskyi <oleksandr_benidovskyi@epam.com>

--- meta-aos-rpi-domd/recipes-core/images/aos-image-rpi.bb
+++ meta-aos-rpi-domd/recipes-core/images/aos-image-rpi.bb
@@ -3,14 +3,26 @@ SUMMARY = "Aos image for Raspberry Pi devices"
 require recipes-core/images/rpi5-image-xt-domd.bb
 require recipes-core/images/aos-image.inc
 
+inherit extrausers
+
 IMAGE_INSTALL:append = " \
     packagegroup-core-ssh-openssh \
     tzdata \
+    sudo \
 "
 
 IMAGE_INSTALL:append = " \
     aos-messageproxy \
 "
+
+EXTRA_USERS_PARAMS = " \
+    useradd aos; \
+    usermod -a -G sudo aos; \
+"
+
+do_install:append() {
+    sed -i 's/^#\s*\(%sudo\s*ALL=(ALL:ALL)\s*ALL\)/\1/'  ${D}/${sysconfdir}/sudoers
+}
 
 # Set fixed rootfs size
 IMAGE_ROOTFS_SIZE ?= "1048576"
--- meta-aos-rpi-install/recipes-core/initrdscripts/files/install_script.sh
+++ meta-aos-rpi-install/recipes-core/initrdscripts/files/install_script.sh
@@ -16,9 +16,9 @@ sleep 1
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 mkdir -p /proc /sys /dev
-mount -t proc proc /proc || echo "Failed to mount /proc"
-mount -t sysfs sysfs /sys || echo "Failed to mount /sys"
-mount -t devtmpfs dev /dev || echo "Failed to mount /dev"
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs dev /dev
 
 echo "Initializing the file system..."
 
@@ -58,32 +58,63 @@ wait_for_block_device() {
     return 0
 }
 
+update_user() {
+    mkdir -p /sd1 /flash1
+    mount -t auto /dev/mmcblk0p1 /sd1/
+
+    if [ -f /sd1/firstrun.sh ]; then
+        echo "Adding user provided by Raspberry Pi Imager..."
+
+        mount -t auto "/dev/${BLOCK_DEVICE_ROOTFS_PARTITION}" /flash1/
+        output=$(sed -n "s/.*userconf '\(.*\)' '\(.*\)'.*/\1 \2/p" /sd1/firstrun.sh)
+        user=$(echo "$output" | awk '{print $1}')
+
+        if [ -n "$user" ]; then
+            password=$(echo "$output" | awk '{print $2}')
+            sed -i "s/^aos:/$user:/; s/\/home\/aos/\/home\/$user/" "/flash1/etc/passwd"
+            escaped_password=$(printf '%s\n' "$password" | sed 's/[&/\]/\\&/g')
+            sed -i "s/^aos:[^:]*/$user:$escaped_password/" "/flash1/etc/shadow"
+            if [ -d "/flash1/home/aos" ]; then
+                mv "/flash1/home/aos" "/flash1/home/$user"
+            fi
+        else
+            echo "No valid user found"
+        fi
+
+        umount /flash1
+    fi
+    
+    umount /sd1
+}
+
 read_args
 
-BLOCK_DEVICE_PARTITION="${BLOCK_DEVICE}3"
+BLOCK_DEVICE_ROOTFS_PARTITION="${BLOCK_DEVICE}1"
+BLOCK_DEVICE_AOS_PARTITION="${BLOCK_DEVICE}3"
 BLOCK_DEVICE="${BLOCK_DEVICE%p}"
 
 wait_for_block_device "$BLOCK_DEVICE"
 
 echo "Preparing the SD card..."
 
-mkdir -p /flash /sd
-mount -t auto /dev/mmcblk0p2 /sd
+mkdir -p /sd2 /flash3
+mount -t auto /dev/mmcblk0p2 /sd2
 
 echo "Flashing root device..."
 
-gunzip -c /sd/rootfs.img.gz | dd of="/dev/$BLOCK_DEVICE" bs=8096 || echo "Failed to write rootfs.img"
-mkfs.ext4 -F -E lazy_journal_init=1 "/dev/$BLOCK_DEVICE_PARTITION" || echo "Failed to format /dev/$BLOCK_DEVICE_PARTITION"
+gunzip -c /sd2/rootfs.img.gz | dd of="/dev/$BLOCK_DEVICE" bs=8096
+mkfs.ext4 -F -E lazy_journal_init=1 "/dev/$BLOCK_DEVICE_AOS_PARTITION"
 
-mount -t auto "/dev/$BLOCK_DEVICE_PARTITION" /flash || echo "Failed to mount /dev/$BLOCK_DEVICE_PARTITION"
+mount -t auto "/dev/$BLOCK_DEVICE_AOS_PARTITION" /flash3
+
+update_user
 
 echo "Flashing SD card..."
 
-cp /sd/boot.img.gz /flash || echo "Failed to copy boot.img"
-umount /sd
-gunzip -c /flash/boot.img.gz | dd of=/dev/mmcblk0 bs=8096 || echo "Failed to write boot.img"
-
-rm /flash/boot.img.gz
+cp /sd2/boot.img.gz /flash3 
+umount /sd2
+gunzip -c /flash3/boot.img.gz | dd of=/dev/mmcblk0 bs=8096
+rm /flash3/boot.img.gz
 
 echo
 echo "********************************************************************************"
